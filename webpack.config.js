c5450ac1775e5f168f53ad5aa9755dbce643b045
Your Name <you@example.com>
2021-10-08 02:31:18 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+
+module.exports = {
+  entry: './src/js/index.js',
+  
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
+  
+};
